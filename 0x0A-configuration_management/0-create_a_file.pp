d612692911c392fb1f0fcb5a0979514e7629dfbb
Anwar Endris <anwarendo@gmail.com>
2023-01-10 16:01:32 +0300

    Add readme and task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# creates the file "/tmp/school" and changes it's file owner, group, and access mode accordingly
+file { '/tmp/school':
+  ensure  => file,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
+}
